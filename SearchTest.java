1da808a60a94e2d641cb4e13cdfac2a7a3de85c1
BlackFish721 <150912485+BlackFish721@users.noreply.github.com>
2024-11-23 14:56:58 +0400

    hot fix

--- SearchTest.java
+++ SearchTest.java
@@ -8,6 +8,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTest {
     @Test
     void successfulSearchTest(){
+        Configuration.pageLoadStrategy = "eager";
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://ru.selenide.org"));
